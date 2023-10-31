1a4d211352c08d3d34d3f06baed25a130562df05
skye <skye53653@gmail.com>
2023-10-31 16:52:18 -0400

    add dataset.py

--- dataset.py
+++ dataset.py
@@ -1,89 +1,87 @@
+## Author: Lishuo Pan 2020/4/18
+
 import torch
 from torchvision import transforms
 import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 import h5py
 import numpy as np
-from utils import *
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-
 class BuildDataset(torch.utils.data.Dataset):
     def __init__(self, path):
-        #############################################
-        # TODO Initialize  Dataset
-        #############################################
+        # TODO: load dataset, make mask list
+        imgs_path, masks_path, labels_path, bboxes_path = path
+        
+        self.imgs = h5py.File(imgs_path, 'r')['data']
 
+        self.masks = h5py.File(masks_path, 'r')['data']
 
-    # In this function for given index we rescale the image and the corresponding  masks, boxes
-    # and we return them as output
+        self.transform = transforms.Compose([
+            transforms.Resize((800, 1066)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            transforms.Pad((11, 0)) 
+        ])
+        self.labels = np.load(labels_path, allow_pickle=True)
+        self.bboxes = np.load(bboxes_path, allow_pickle=True)
+        self.cumulative_labels = np.cumsum([0] + [len(label) for label in self.labels])
+        
     # output:
         # transed_img
         # label
         # transed_mask
         # transed_bbox
-        # index
     def __getitem__(self, index):
-        ################################
-        # TODO return transformed images,labels,masks,boxes,index
-        ################################
-        assert transed_img.shape == (3,800,1088)
+        # TODO: __getitem__
+        raw_img = self.imgs[index]
+        mask_init_index = self.cumulative_labels[index]
+        mask_end_index = self.cumulative_labels[index+1] 
+        raw_mask = self.masks[mask_init_index:mask_end_index]  
+        raw_bbox = self.bboxes[index]
+        label = self.labels[index]
+        # Preprocess the raw data
+        transed_img, transed_mask, transed_bbox = self.pre_process_batch(raw_img, raw_mask, raw_bbox)
+        label = torch.tensor(label)
+        # check flag
+        assert transed_img.shape == (3, 800, 1088)
         assert transed_bbox.shape[0] == transed_mask.shape[0]
-
-        
         return transed_img, label, transed_mask, transed_bbox, index
-
-
-
-    # This function preprocess the given image, mask, box by rescaling them appropriately
-    # output:
-    #        img: (3,800,1088)
-    #        mask: (n_box,800,1088)
-    #        box: (n_box,4)
-    def pre_process_batch(self, img, mask, bbox):
-        #######################################
-        # TODO apply the correct transformation to the images,masks,boxes
-        ######################################
-
-        assert img.squeeze(0).shape == (3, 800, 1088)
-        assert bbox.shape[0] == mask.squeeze(0).shape[0]
-
-        return img.squeeze(0), mask.squeeze(0), bbox
-    
-
-    
+        
+        
     def __len__(self):
-        return len(self.imgs_data)
+        #return len(self.imgs_data)
+        return len(self.labels)
+    # This function take care of the pre-process of img,mask,bbox
+    # in the input mini-batch
+    # input:
+        # img: 3*300*400
+        # mask: 3*300*400
+        # bbox: n_box*4
+    def pre_process_batch(self, img, mask, bbox):
+        # TODO: image preprocess
+        original_width = img.shape[-1]
+        img = img /255.0
+        img = torch.tensor(img, dtype=torch.float32)
+        img = self.transform(img)
 
+        mask = mask.astype(np.float32)
+        mask = torch.tensor(mask)
+        mask = transforms.Resize((800, 1066), interpolation=transforms.InterpolationMode.NEAREST)(mask)
+        mask = transforms.Pad((11, 0))(mask)    
+       
+        scale = 1066 / original_width 
+        bbox = torch.tensor(bbox)
+        bbox *= scale
+        bbox[:, [0, 2]] += 11  
+    
+        # check flag
+        assert img.shape == (3, 800, 1088)
+        assert bbox.shape[0] == mask.shape[0]
+        return img, mask, bbox
 
+def collate_fn(batch):
+    images, labels, masks, bounding_boxes, indices = list(zip(*batch))
+    return torch.stack(images), labels, masks, bounding_boxes, indices 
+    
 
-
-class BuildDataLoader(torch.utils.data.DataLoader):
-    def __init__(self, dataset, batch_size, shuffle, num_workers):
-        self.dataset = dataset
-        self.batch_size = batch_size
-        self.shuffle = shuffle
-        self.num_workers = num_workers
-
-
-    # output:
-    #  dict{images: (bz, 3, 800, 1088)
-    #       labels: list:len(bz)
-    #       masks: list:len(bz){(n_obj, 800,1088)}
-    #       bbox: list:len(bz){(n_obj, 4)}
-    #       index: list:len(bz)
-    def collect_fn(self, batch):
-
-        return out_batch
-
-
-    def loader(self):
-        return DataLoader(self.dataset,
-                          batch_size=self.batch_size,
-                          shuffle=self.shuffle,
-                          num_workers=self.num_workers,
-                          collate_fn=self.collect_fn)
-
-
-if __name__ == '__main__':
