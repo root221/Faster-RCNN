204343d0df573efdfc1675d0c48163543aa38f5e
skye <skye53653@gmail.com>
2023-11-01 23:11:48 -0400

    complete the training part of the code for Faster R-CNN

--- BoxHead.py
+++ BoxHead.py
@@ -7,9 +7,11 @@ from torch import optim
 import pytorch_lightning as pl
 from torchvision.models.detection.image_list import ImageList
 from torchvision.ops import roi_align
+from torch.optim.lr_scheduler import StepLR
 from utils import compute_iou
 from faster_rcnn_datamodule import DataModule
 from pretrained_models import pretrained_models_680 
+from pytorch_lightning.loggers import WandbLogger
 class BoxHead(pl.LightningModule):
     def __init__(self,Classes=3,P=7, pretrained_path='checkpoint680.pth'):
         super().__init__()
@@ -28,7 +30,7 @@ class BoxHead(pl.LightningModule):
     
         self.clasifier_head = nn.Sequential(
             nn.Linear(1024, Classes+1), 
-            nn.Softmax(dim=1)
+            #nn.Softmax(dim=1)
         )
         
         self.regressor_head = nn.Sequential(
@@ -43,21 +45,41 @@ class BoxHead(pl.LightningModule):
     #  Output: (make sure the ordering of the proposals are consistent with MultiScaleRoiAlign)
     #       labels: (total_proposals,1) (the class that the proposal is assigned)
     #       regressor_target: (total_proposals,4) (target encoded in the [t_x,t_y,t_w,t_h] format)
-    def create_ground_truth(self,proposals,gt_labels,bboxes):
+    def create_ground_truth(self, proposals, gt_labels, bboxes):
         labels = []
         regressor_targets = []
         batch_size = len(proposals)
         for i in range(batch_size):
             num_proposals = len(proposals[i])
             regressor_target = torch.zeros_like(proposals[i])
-            label = torch.zeros(num_proposals, 1, device=regressor_target.device)
+            label = torch.zeros(num_proposals, 1, device=regressor_target.device, dtype=torch.long)
             ious = compute_iou(proposals[i], bboxes[i]) 
             max_values, max_indices = torch.max(ious, axis=1)
             above_thres_indices = max_values > 0.5
-            label[above_thres_indices] = gt_labels[i][max_indices[above_thres_indices]].view(-1,1).to(torch.float32)
-            regressor_target[above_thres_indices] = bboxes[i][max_indices[above_thres_indices]]
+            label[above_thres_indices] = gt_labels[i][max_indices[above_thres_indices]].view(-1,1)
+            pos_proposals = proposals[i][above_thres_indices] 
+            x_p = (pos_proposals[:, 2] +  pos_proposals[:, 0])/2
+            y_p = (pos_proposals[:, 3] +  pos_proposals[:, 1])/2
+            w_p = pos_proposals[:, 2] -  pos_proposals[:, 0]
+            h_p = pos_proposals[:, 3] - pos_proposals[:, 1]
+            x = (bboxes[i][:, 2] + bboxes[i][:, 0])/2
+            y = (bboxes[i][:, 3] + bboxes[i][:, 1])/2
+            w = bboxes[i][:, 2] - bboxes[i][:, 0] 
+            h = bboxes[i][:, 3] - bboxes[i][:, 1]
+            x = x[max_indices[above_thres_indices]] 
+            y = y[max_indices[above_thres_indices]] 
+            w = w[max_indices[above_thres_indices]] 
+            h = h[max_indices[above_thres_indices]] 
+            t_x = (x - x_p)/w_p
+            t_y = (y - y_p)/h_p 
+            t_w = torch.log(w/w_p)
+            t_h = torch.log(h/h_p)
+            
+            regressor_target[above_thres_indices] = torch.column_stack([t_x, t_y, t_w, t_h])
             labels.append(label)
             regressor_targets.append(regressor_target)
+        labels = torch.vstack(labels)
+        regressor_targets = torch.vstack(regressor_targets)
         return labels,regressor_targets
 
 
@@ -70,7 +92,7 @@ class BoxHead(pl.LightningModule):
     #      P: scalar
     # Output:
     #      feature_vectors: (total_proposals, 256*P*P)  (make sure the ordering of the proposals are the same as the ground truth creation)
-    def MultiScaleRoiAlign(self, fpn_feat_list,proposals,P=7):
+    def MultiScaleRoiAlign(self, fpn_feat_list,proposals, P=7):
         #####################################
         # Here you can use torchvision.ops.RoIAlign check the docs
         #####################################
@@ -80,7 +102,7 @@ class BoxHead(pl.LightningModule):
             width = (proposals[i][:, 2] -  proposals[i][:, 0])
             height = (proposals[i][:, 3] -  proposals[i][:, 1]) 
             k = torch.floor(4 + torch.log2((width*height).sqrt()/224))
-            k = torch.clamp(k, 2)
+            k = torch.clamp(k, 2, 5)
             for j in range(2, 6):
                 feature_vectors.append(roi_align(fpn_feat_list[j-2][i:i+1], [proposals[i][k==j]], P, spatial_scale=self.strides[j-2]))
         feature_vectors = torch.vstack(feature_vectors)
@@ -88,9 +110,8 @@ class BoxHead(pl.LightningModule):
         return feature_vectors
 
 
-
     # This function does the post processing for the results of the Box Head for a batch of images
-    # Use the proposals to distinguish the outputs from each image
+    # Use the proposal to distinguish the outputs from each image
     # Input:
     #       class_logits: (total_proposals,(C+1))
     #       box_regression: (total_proposal,4*C)           ([t_x,t_y,t_w,t_h] format)
@@ -121,11 +142,35 @@ class BoxHead(pl.LightningModule):
     #      loss: scalar
     #      loss_class: scalar
     #      loss_regr: scalar
-    def compute_loss(self,class_logits, box_preds, labels, regression_targets,l=1,effective_batch=150):
-
+    def compute_loss(self,class_logits, box_preds, labels, regression_targets,l=1,effective_batch=100):
+        pos_indices, neg_indices = self.sample_mini_batch(labels, regression_targets, effective_batch)
+        pos_labels = labels[pos_indices]
+        neg_labels = labels[neg_indices]
+        loss_class = nn.CrossEntropyLoss(reduction="sum")(class_logits[pos_indices], pos_labels.squeeze(-1)) 
+        loss_class += nn.CrossEntropyLoss(reduction="sum")(class_logits[neg_indices], neg_labels.squeeze(-1))
+        loss_class /= effective_batch
+       
+        range_tensor = torch.arange(4).expand(pos_labels.shape[0], 4).to(pos_labels.device)
+        indices = (pos_labels - 1)*4 + range_tensor
+        pos_box_preds = torch.gather(box_preds[pos_indices], 1, indices) 
+        loss_regr = torch.nn.SmoothL1Loss(reduction="sum")(regression_targets[pos_indices], pos_box_preds)
+        num_pos = len(pos_indices)
+        loss_regr = loss_regr / 4
+        loss = loss_class + loss_regr
         return loss, loss_class, loss_regr
 
 
+    def sample_mini_batch(self, ground_clas, ground_coord, effective_batch):
+        pos_indices = torch.where(ground_clas > 0)[0]
+        neg_indices = torch.where(ground_clas == 0)[0]
+        sample_size = int(3*effective_batch/4)
+        if len(pos_indices) > sample_size:
+            rand_indices = torch.randperm(pos_indices.shape[0])[:sample_size]
+            pos_indices = pos_indices[rand_indices] 
+        neg_sample_size = effective_batch - len(pos_indices)
+        rand_indices = torch.randperm(neg_indices.shape[0])[:neg_sample_size]
+        neg_indices = neg_indices[rand_indices] 
+        return pos_indices, neg_indices 
 
     # Forward the pooled feature vectors through the intermediate layer and the classifier, regressor of the box head
     # Input:
@@ -136,47 +181,81 @@ class BoxHead(pl.LightningModule):
     #        box_pred:     (total_proposals,4*C)
     def forward(self, feature_vectors):
         intermediate_output = self.intermediate_layer(feature_vectors)
-        class_logits = self.clasifier_head(intermediate_layer)
-        box_pred = self.regressor_head(intermediate_layer)
+        class_logits = self.clasifier_head(intermediate_output)
+        box_pred = self.regressor_head(intermediate_output)
         return class_logits, box_pred
-    
-    def training_step(self, batch, batch_idx):
-        images, labels, _, bboxes, indices = batch
-        batch_size = len(images) 
-        self.backbone.eval()
-        self.rpn.eval()
-        # Take the features from the backbone
-        backout = self.backbone(images)
-
-        # The RPN implementation takes as first argument the following image list
-        im_lis = ImageList(images, [(800, 1088)]*images.shape[0])
-        # Then we pass the image list and the backbone output through the rpn
-        rpnout = self.rpn(im_lis, backout)
-
-    
-        keep_topK = 200
-        # A list of proposal tensors: list:len(bz){(keep_topK,4)}
-        proposals=[proposal[0:keep_topK,:] for proposal in rpnout[0]]
-        # A list of features produces by the backbone's FPN levels: list:len(FPN){(bz,256,H_feat,W_feat)}
-        fpn_feat_list= list(backout.values())
-       
+   
+    def sort_proposals(self, proposals):
+        batch_size = len(proposals)
         for i in range(batch_size):
             width = proposals[i][:, 2] - proposals[i][:, 0]
             height = proposals[i][:, 3] - proposals[i][:, 1]
             area = width * height
             sorted_indices = torch.argsort(area) 
             proposals[i] = proposals[i][sorted_indices]
+        return proposals
         
+    def training_step(self, batch, batch_idx):
+        images, labels, bboxes, indices = batch
+       
+        with torch.no_grad():
+            self.backbone.eval()
+            self.rpn.eval()
+            backout = self.backbone(images)
+
+            im_lis = ImageList(images, [(800, 1088)]*images.shape[0])
+            rpnout = self.rpn(im_lis, backout)
+
+    
+        keep_topK = 150
+        # A list of proposal tensors: list:len(bz){(keep_topK,4)}
+        proposals=[proposal[0:keep_topK,:] for proposal in rpnout[0]]
+        # A list of features produces by the backbone's FPN levels: list:len(FPN){(bz,256,H_feat,W_feat)}
+        fpn_feat_list= list(backout.values())
+       
+        proposals = self.sort_proposals(proposals)
             
-        self.create_ground_truth(proposals, labels, bboxes)
+        labels, regressor_targets = self.create_ground_truth(proposals, labels, bboxes)
     
-        feature_vectors = self.MultiScaleRoiAlign(fpn_feat_list,proposals,P=7)
-        class_logits, box_pred = self(feature_vectors)
-   
+        feature_vectors = self.MultiScaleRoiAlign(fpn_feat_list,proposals, P=7)
+        class_logits, box_preds = self(feature_vectors)
+        loss, loss_c, loss_r = self.compute_loss(class_logits, box_preds, labels, regressor_targets) 
+        self.log("train/loss", loss, on_step=False, on_epoch=True, logger=True)
+        self.log("train/classifier's loss", loss_c, on_step=False, on_epoch=True, logger=True)
+        self.log("train/regressor's loss", loss_r, on_step=False, on_epoch=True, logger=True)
+        return loss
+        
+    def validation_step(self, batch, batch_idx):
+        images, labels, bboxes, indices = batch
+       
+        backout = self.backbone(images)
+
+        im_lis = ImageList(images, [(800, 1088)]*images.shape[0])
+        rpnout = self.rpn(im_lis, backout)
+
     
+        keep_topK = 150
+        # A list of proposal tensors: list:len(bz){(keep_topK,4)}
+        proposals=[proposal[0:keep_topK,:] for proposal in rpnout[0]]
+        # A list of features produces by the backbone's FPN levels: list:len(FPN){(bz,256,H_feat,W_feat)}
+        fpn_feat_list= list(backout.values())
+       
+        proposals = self.sort_proposals(proposals)
+            
+        labels, regressor_targets = self.create_ground_truth(proposals, labels, bboxes)
+    
+        feature_vectors = self.MultiScaleRoiAlign(fpn_feat_list,proposals, P=7)
+        class_logits, box_preds = self(feature_vectors)
+        loss, loss_c, loss_r = self.compute_loss(class_logits, box_preds, labels, regressor_targets) 
+        self.log("val/loss", loss, on_step=False, on_epoch=True, logger=True)
+        self.log("val/classifier's loss", loss_c, on_step=False, on_epoch=True, logger=True)
+        self.log("val/regressor's loss", loss_r, on_step=False, on_epoch=True, logger=True)
+        
+        return loss
     def configure_optimizers(self):
-        optimizer = optim.Adam(self.parameters(), lr=1e-4)
-        return optimizer
+        optimizer = optim.Adam(self.parameters(), lr=1e-3)
+        scheduler = StepLR(optimizer, step_size=10, gamma=0.99)
+        return {"optimizer": optimizer, "lr_scheduler": scheduler} 
      
 if __name__ == '__main__':
     imgs_path = '../partA/data/hw3_mycocodata_img_comp_zlib.h5'
@@ -187,5 +266,6 @@ if __name__ == '__main__':
     datamodule = DataModule(paths) 
     box_head= BoxHead() 
     
-    trainer = pl.Trainer(max_epochs=40, devices=1)
+    wandb_logger = WandbLogger(name='box_head', project="faster_rcnn", log_model=True)
+    trainer = pl.Trainer(max_epochs=40, devices=1, logger=wandb_logger)
     trainer.fit(box_head, datamodule=datamodule)
